1a12aad8dac2707ca73006fa9014cf704fa77933
ahaw24-prog <ahaw24@gmail.com>
2026-08-31 14:39:41 +0200

    Refactor message handling and origin validation

--- login_script.js
+++ login_script.js
@@ -5,22 +5,50 @@ if (!process.env.ORIGINS.match(REQUIRED_ORIGIN_PATTERN)) {
   throw new Error('process.env.ORIGINS MUST be comma separated list \
     of origins that login can succeed on.')
 }
+const origins = process.env.ORIGINS.split(',')
 
 module.exports = (oauthProvider, message, content) => `
 <script>
 (function() {
-  var target = window.opener || (window.parent !== window ? window.parent : null);
-  if (target) {
-    console.log("Sending token to main window...");
-    target.postMessage(
-      'authorization:${oauthProvider}:${message}:${JSON.stringify(content)}',
-      '*'
-    );
-    setTimeout(function() {
-      window.close();
-    }, 300);
-  } else {
-    console.error("No opener window found.");
+  function contains(arr, elem) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf('*') >= 0) {
+        const regex = new RegExp(arr[i].replaceAll('.', '\\\\.').replaceAll('*', '[\\\\w_-]+'))
+        if (elem.match(regex) !== null) {
+          return true;
+        }
+      } else {
+        if (arr[i] === elem) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  var targetWindow = window.opener || (window.parent !== window ? window.parent : null);
+
+  function recieveMessage(e) {
+    console.log("recieveMessage %o", e)
+    if (!contains(${JSON.stringify(origins)}, e.origin.replace('https://', 'http://').replace('http://', ''))) {
+      console.log('Invalid origin: %s', e.origin);
+      return;
+    }
+    if (targetWindow) {
+      targetWindow.postMessage(
+        'authorization:${oauthProvider}:${message}:${JSON.stringify(content)}',
+        '*'
+      );
+      setTimeout(function() {
+        window.close();
+      }, 300);
+    }
+  }
+
+  window.addEventListener("message", recieveMessage, false);
+  console.log("Sending initial handshake message: %o", "${oauthProvider}");
+  if (targetWindow) {
+    targetWindow.postMessage("authorizing:${oauthProvider}", "*");
   }
 })()
 </script>`
